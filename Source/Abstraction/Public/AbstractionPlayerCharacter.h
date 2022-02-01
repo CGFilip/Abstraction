afbe4a3cdf15cc148493076db1f66daa1c77e7eb
CGFilip <filipkrstevski@cgspectrum.edu.au>
2022-02-01 12:59:09 -0500

    Module 6
    Dealing Damage

---
+++ Source/Abstraction/Public/AbstractionPlayerCharacter.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbstractionPlayerCharacter.generated.h"
+
+class UHealthComponent;
+
+UCLASS()
+class ABSTRACTION_API AAbstractionPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAbstractionPlayerCharacter();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Called when the actor falls out of the world 'safety'
+	virtual void FellOutOfWorld(const class UDamageType& dmgType) override;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void OnDeath(bool IsFellOut);
+
+	UPROPERTY(EditAnywhere)
+	UHealthComponent* HealthComponent;
+};
